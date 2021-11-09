e286da460b4fe5dd2361aab4500bedefd683818a
Brent Haines <thebrenthaines@yahoo.com>
2021-11-08 18:09:10 -0800

    Dependencies for NLP tools.

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 import Dependencies._
 
-ThisBuild / scalaVersion     := "2.13.6"
+ThisBuild / scalaVersion     := "2.12.15"
 ThisBuild / version          := "0.1.0-SNAPSHOT"
 ThisBuild / organization     := "io.github.intelligence4s"
 ThisBuild / organizationName := "Intelligence4s"
@@ -11,18 +11,18 @@ ThisBuild / versionScheme := Some("early-semver")
 lazy val root = (project in file("."))
   .settings(
     name := "i4s-symbolic",
-    resolvers += "Sonatype OSS Snapshots" at "https://s01.oss.sonatype.org/content/repositories/snapshots",
-    libraryDependencies ++= Seq(
-      akka,
-      akkaHttp,
-      akkaStream,
-      akkaTest,
-      logback,
-      neo4jDriver,
-      neo4sCypherDsl,
-      neo4sQuerySupport,
-      scalaTest
+    resolvers ++= Seq(
+      "Sonatype OSS Snapshots" at "https://s01.oss.sonatype.org/content/repositories/snapshots",
+      ("Artifactory" at "http://artifactory.cs.arizona.edu:8081/artifactory/sbt-release").withAllowInsecureProtocol(true)
     ),
+    libraryDependencies ++=
+      akkaDependencies ++
+      neo4jDependencies ++
+      nlpDependencies ++
+      Seq(
+        logback,
+        scalaTest
+      ),
     licenses := Seq("MIT" -> url("https://www.mit.edu/~amini/LICENSE.md")),
     homepage := Some(url("https://github.com/intelligence4s/i4s-symbolic")),
     pomIncludeRepository := (_ => false),
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,17 +1,37 @@
 import sbt._
 
 object Dependencies {
-  lazy val akkaVersion = "2.6.17"
-  val akkaHttpVersion = "10.2.7"
 
-  lazy val akka = "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion
-  lazy val akkaStream = "com.typesafe.akka" %% "akka-stream" % akkaVersion
-  lazy val akkaHttp = "com.typesafe.akka" %% "akka-http" % akkaHttpVersion
-  lazy val akkaTest = "com.typesafe.akka" %% "akka-actor-testkit-typed" % akkaVersion % Test
+  lazy val akkaDependencies = {
+    val akkaVersion = "2.6.17"
+    val akkaHttpVersion = "10.2.7"
+
+    Seq(
+      "com.typesafe.akka" %% "akka-actor-typed" % akkaVersion,
+      "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+      "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+      "com.typesafe.akka" %% "akka-actor-testkit-typed" % akkaVersion % Test
+    )
+  }
+
   lazy val logback = "ch.qos.logback" % "logback-classic" % "1.2.6"
-  lazy val neo4jDriver = "org.neo4j.driver" % "neo4j-java-driver" % "4.3.4"
-  lazy val neo4sCypherDsl = "io.github.neo4s" %% "neo4s-cypher-dsl" % "0.5.0-SNAPSHOT"
-  lazy val neo4sQuerySupport = "io.github.neo4s" %% "neo4s-query-support" % "0.1.0-SNAPSHOT"
+
+  lazy val neo4jDependencies = Seq(
+    "org.neo4j.driver" % "neo4j-java-driver" % "4.3.4",
+    "io.github.neo4s" %% "neo4s-cypher-dsl" % "0.5.0-SNAPSHOT",
+    "io.github.neo4s" %% "neo4s-query-support" % "0.1.0-SNAPSHOT"
+  )
+
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.9" % Test
+
+  lazy val nlpDependencies = {
+    val procVer = "8.4.6"
+    Seq(
+      "org.clulab" %% "processors-main" % procVer,
+      "org.clulab" %% "processors-corenlp" % procVer,
+      "edu.stanford.nlp" % "stanford-corenlp" % "4.3.1",
+      "edu.stanford.nlp" % "stanford-corenlp" % "4.3.1" classifier "models"
+    )
+  }
 
 }

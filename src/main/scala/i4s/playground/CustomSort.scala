0145f6327f54bfa01864a828e89f0c62fc4c1696
Reagan <rwhaines@wisc.edu>
2022-05-24 17:26:29 -0700

    implemented sorting algorithms.

--- src/main/scala/i4s/playground/CustomSort.scala
+++ src/main/scala/i4s/playground/CustomSort.scala
@@ -1,8 +1,22 @@
 package i4s.playground
 
 object CustomSort {
-  def sortInts(ints: List[Int]): List[Int] = ints
+  def sortInts(ints: List[Int]): List[Int] = {
+    var sorted = List[Int]()
+    for(i <- 0 until ints.length){
+      val (less, greater) = sorted.span(_ < ints(i))
+      sorted = less ::: (ints(i) :: greater)
+    }
+    sorted
+  }
 
-  def sort[T](values: List[T])(implicit ord: Ordering[T]): List[T] = values
+  def sort[T](values: List[T])(implicit ord: Ordering[T]): List[T] = {
+    var sorted = List[T]()
+    for(i <- 0 until values.length){
+      val (less, greater) = sorted.span(ord.lt(_, values(i)))
+      sorted = less ::: (values(i) :: greater)
+    }
+    sorted
+  }
 
 }

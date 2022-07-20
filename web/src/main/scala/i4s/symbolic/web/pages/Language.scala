a2ff2283eaff378273773081ea34c8ee6db7c9a1
Brent Haines <brent@aifoundation.com>
2022-07-19 22:03:50 -0700

    Hook up server analysis to language UI.

--- web/src/main/scala/i4s/symbolic/web/pages/Language.scala
+++ web/src/main/scala/i4s/symbolic/web/pages/Language.scala
@@ -1,9 +1,11 @@
 package i4s.symbolic.web.pages
 
+import i4s.symbolic.language.grammar.TokenGraph
 import i4s.symbolic.web.components.{Banner, SentenceGraph, icons}
 import i4s.symbolic.web.graphql.SymbolicQueries
 import i4s.symbolic.web.model.JSTokenGraph
-import slinky.core.FunctionalComponent
+import org.scalajs.dom.{Event, KeyboardEvent, html}
+import slinky.core.{FunctionalComponent, SyntheticEvent}
 import slinky.core.annotations.react
 import slinky.core.facade.Hooks.{useEffect, useState}
 import slinky.core.facade.ReactElement
@@ -17,21 +19,49 @@ import slinky.web.html._
   case class Props()
   val component = FunctionalComponent[Props] { props =>
     val (sentence, setSentence) = useState[Option[String]](None)
-    val (dependencyGraph, setDependencyGraph) = useState[Option[JSTokenGraph]](None)
-    val (loading, setLoading) = useState[Boolean](true)
+    val (dependencyGraph, setDependencyGraph) = useState[Option[TokenGraph]](None)
+    val (loading, setLoading) = useState[Boolean](false)
 
     useEffect(() => sentence.foreach { s =>
+      println(s"""Sending "$s" to server for analysis...""")
+
       SymbolicQueries.grammarFor(s).map { graphList =>
-        setDependencyGraph(graphList.map(_.toJs).headOption)
+        println(s"Result from query -- ${graphList.headOption}")
+        setDependencyGraph(graphList.headOption)
       }
     }, Seq(sentence))
 
-    val graphOrWaitElement: ReactElement = if (loading) {
-      icons.LoadingIcon
-    } else {
-      dependencyGraph
-        .map(jsTokenGraph => SentenceGraph(graph = jsTokenGraph).asInstanceOf[ReactElement])
-        .getOrElse(div(className := "flex p-10"))
+    val graphOrWaitElement: ReactElement = {
+      if (loading) {
+        div(className := "flex mx-4 border rounded")(icons.LoadingIcon)
+      } else {
+        if (dependencyGraph.isDefined) {
+          div(className := "flex mx-4 bg-blue-200 boerder rounded")(
+            dependencyGraph
+            .map(tokenGraph => SentenceGraph(graph = tokenGraph.toJs).withKey("sentence-graph"))
+            .getOrElse(div(className := "flex p-10"))
+          )
+        } else {
+          div(className := "flex mx-4 boerder rounded")
+        }
+      }
+    }
+
+    def handleAnalyzeClick(e: SyntheticEvent[html.Anchor, Event]): Unit = {
+      val input = e.currentTarget.parentElement.querySelector("#sentence-input").asInstanceOf[html.Input]
+      analyzeSentence(input.value)
+    }
+
+    def handleAnalizeEnterKey(e: SyntheticEvent[html.Input, KeyboardEvent]): Unit = {
+      val input = e.currentTarget
+      if (e.nativeEvent.key == "Enter") {
+        analyzeSentence(input.value)
+      }
+    }
+
+    def analyzeSentence(sentenceText: String): Unit = {
+      println(s"Analyzing = ${sentenceText}")
+      if (!loading) setSentence(Some(sentenceText))
     }
 
     div(
@@ -40,15 +70,13 @@ import slinky.web.html._
 
       div(className := "flex p-4")(
         div(className := "flex w-full bg-gray-200 border rounded")(
-          input(`type` := "text", className := "appearance-none block w-full bg-gray-200 text-gray-700 border rounded my-2 ml-2 py-1 px-4 leading-tight focus:outline-none focus:bg-white", placeholder := "Sentence to analyze...")(),
-          a(className := "flex-shrink-0 text-sm text-gray-500 border rounded hover:bg-gray-500 hover:text-white p-1 m-auto mx-1", href := "#")(
+          input(`type` := "text", id := "sentence-input", className := "appearance-none block w-full bg-gray-200 text-gray-700 border rounded my-2 ml-2 py-1 px-4 leading-tight focus:outline-none focus:bg-white", placeholder := "Sentence to analyze...", onKeyUp := (handleAnalizeEnterKey(_)))(),
+          a(className := "flex-shrink-0 text-sm text-gray-500 border rounded hover:bg-gray-500 hover:text-white p-1 m-auto mx-1", href := "#", onClick := (handleAnalyzeClick(_)))(
             icons.PlayIcon("w-6 h-6 m-auto")
           )
         )
       ),
-      div(className := "flex p-4")(
-        graphOrWaitElement
-      )
+      graphOrWaitElement
     )
   }
 }

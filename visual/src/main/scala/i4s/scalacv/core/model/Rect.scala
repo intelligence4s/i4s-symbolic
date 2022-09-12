c3d20e6655cebee2995c71a351c7ac941211893d
Brent Haines <brent@aifoundation.com>
2022-09-12 11:12:09 -0700

    Added simple manipulation methods to RectLike and common decendants

--- visual/src/main/scala/i4s/scalacv/core/model/Rect.scala
+++ visual/src/main/scala/i4s/scalacv/core/model/Rect.scala
@@ -1,13 +1,23 @@
 package i4s.scalacv.core.model
 
+import i4s.scalacv.core.model.Math.NumberLike
+
 object Rect {
   def apply(r: org.bytedeco.opencv.opencv_core.Rect): Rect = Rect(r.x,r.y,r.width,r.height)
   def apply(ul: Point, lr: Point): Rect = Rect(ul.x,ul.y,math.abs(lr.x-ul.x),math.abs(lr.y-ul.y))
 
   import scala.language.implicitConversions
   implicit def r2r(r: org.bytedeco.opencv.opencv_core.Rect): Rect = apply(r)
+  implicit def rl2r(rl: RectLike[Int]): Rect = rl.asInstanceOf[Rect]
 }
 
 case class Rect(override val x: Int, override val y: Int, override val width: Int, override val height: Int) extends org.bytedeco.opencv.opencv_core.Rect(x,y,width,height) with RectLike[Int] {
   override def construct(x: Int, y: Int, width: Int, height: Int): RectLike[Int] = Rect(x,y,width,height)
+
+  override def moveTo(p: PointLike[Int]): Rect = super.moveTo(p)
+  override def scaleBy(sz: SizeLike[Int])(implicit ev: NumberLike[Int]): Rect = super.scaleBy(sz)
+  override def shiftBy(sz: SizeLike[Int])(implicit ev: NumberLike[Int]): Rect = super.shiftBy(sz)
+
+  override def tl(): Point = Point(super.tl)
+  override def br(): Point = Point(super.br)
 }
--- visual/src/main/scala/i4s/scalacv/core/model/RectLike.scala
+++ visual/src/main/scala/i4s/scalacv/core/model/RectLike.scala
@@ -3,12 +3,16 @@ package i4s.scalacv.core.model
 import i4s.scalacv.core.model.Math.NumberLike
 
 trait RectLike[T] {
-  protected val x: T
-  protected val y: T
-  protected val width: T
-  protected val height: T
+  val x: T
+  val y: T
+  val width: T
+  val height: T
 
   def construct(x: T, y: T, width: T, height: T): RectLike[T]
+
+  def moveTo(p: PointLike[T]): RectLike[T] = construct(p.x,p.y,width,height)
+  def shiftBy(sz: SizeLike[T])(implicit ev: NumberLike[T]): RectLike[T] = construct(ev.plus(x,sz.width), ev.plus(y,sz.height), width, height)
+  def scaleBy(sz: SizeLike[T])(implicit ev: NumberLike[T]): RectLike[T] = construct(x,y,ev.multiply(width,sz.width),ev.multiply(height,sz.height))
 
   def canEqual(that: Any): Boolean = that.isInstanceOf[RectLike[T]]
 
@@ -24,6 +28,6 @@ trait RectLike[T] {
 
   override def hashCode(): Int = 1 << 32 * x.hashCode() + y.hashCode()
 
-  override def toString: String = s"($x,$y)"
+  override def toString: String = s"($x,$y,$width,$height)"
 
 }

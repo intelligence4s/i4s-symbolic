1202f202a3a06f9eb9b7784ba2e801148060fe96
Brent Haines <brent@aifoundation.com>
2022-09-17 12:34:44 -0700

    Some additional testing for MappedMat

--- visual/src/test/scala/i4s/scalacv/core/model/mats/MappedMatSpec.scala
+++ visual/src/test/scala/i4s/scalacv/core/model/mats/MappedMatSpec.scala
@@ -2,7 +2,7 @@ package i4s.scalacv.core.model.mats
 
 import i4s.scalacv.core.model.{Point, Rect, Scalar}
 import i4s.scalacv.core.types.{MatTypes, Types}
-import org.bytedeco.javacpp.indexer.{IntIndexer, UByteIndexer}
+import org.bytedeco.javacpp.indexer.{DoubleIndexer, IntIndexer, UByteIndexer}
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.wordspec.AnyWordSpec
 
@@ -71,14 +71,33 @@ class MappedMatSpec extends AnyWordSpec with Matchers {
       mat.get(0) shouldBe Rect(128,128,128,128)
     }
 
-    "Put/Get Scalar values to/from a Byte Map" in {
-      val mat = MappedMat[Scalar,Int](50,50,Some(Types.Cv8U),Some(3))
+    "Put/Get Scalar values to/from a 3 channel UByte Mat" in {
+      val mat = MappedMat[Scalar,Int](50,50,Some(Types.Cv8U),Some(3),Scalar.Red)
+
+      mat.get(0,0) shouldBe Scalar.Red
+      mat.put(0,0,Scalar.White)
+
+      mat.get(0,0) shouldBe Scalar.White
+
+      val indexer = mat.createIndexer().asInstanceOf[UByteIndexer]
+      val values = new Array[Int](3)
+      indexer.get(Array[Long](0,0,0),values)
+      values shouldBe Array[Int](255,255,255)
 
     }
 
-    "Put/Get Scalar values to/from a Double Map" in {
-      val mat = MappedMat[Scalar,Double](50,50,Some(Types.Cv64F),Some(3))
+    "Put/Get Scalar values to/from a 4 channel Double Mat" in {
+      val mat = MappedMat[Scalar,Double](50,50,Some(Types.Cv64F),Some(4),Scalar(0,0,0,0))
 
+      mat.get(0,0) shouldBe Scalar(0,0,0,0)
+      mat.put(0,0,Scalar(64,128,192,256))
+
+      mat.get(0,0) shouldBe(Scalar(64,128,192,256))
+
+      val indexer = mat.createIndexer().asInstanceOf[DoubleIndexer]
+      val values = new Array[Double](4)
+      indexer.get(Array[Long](0,0,0),values)
+      values shouldBe Array[Double](64,128,192,256)
     }
   }
 }
